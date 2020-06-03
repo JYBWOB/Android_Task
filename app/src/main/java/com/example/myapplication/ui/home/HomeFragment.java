8b0983374378947dcc77c268de5f021991dc05b1
奕兵 <215370196@qq.com>
2020-06-03 18:24:53 +0800

    lesson card in

--- app/src/main/java/com/example/myapplication/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/myapplication/ui/home/HomeFragment.java
@@ -5,7 +5,10 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -14,18 +17,31 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.myapplication.R;
+import com.example.myapplication.ui.home.Course;
 
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
 
+    View root;
+
+    //星期几
+    private RelativeLayout day;
+
+    LinearLayout leftViewLayout;
+    LayoutInflater inflater;
+
+    int currentCoursesNumber = 0;
+    int maxCoursesNumber = 0;
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
+        this.inflater = inflater;
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel.class);
 
-        View root = inflater.inflate(R.layout.fragment_home, container, false);
+        root = inflater.inflate(R.layout.fragment_home, container, false);
 
         homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
@@ -33,6 +49,78 @@ public class HomeFragment extends Fragment {
 //                textView.setText(s);
             }
         });
+
+
+        leftViewLayout = root.findViewById(R.id.left_view_layout);
+
+        Course course = new Course(
+                "大学物理",
+                "李娟",
+                "B201",
+                1,
+                7,
+                9);
+        createLeftView(course);
+        createItemCourseView(course);
+
         return root;
+    }
+
+    //创建"第几节数"视图
+    private void createLeftView(Course course) {
+        int endNumber = course.getEnd();
+        if (endNumber > maxCoursesNumber) {
+            for (int i = 0; i < endNumber-maxCoursesNumber; i++) {
+                View view = inflater.inflate(R.layout.left_view, null);
+                LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(110,180);
+                view.setLayoutParams(params);
+
+                TextView text = view.findViewById(R.id.class_number_text);
+                text.setText(String.valueOf(++currentCoursesNumber));
+
+                leftViewLayout.addView(view);
+            }
+            maxCoursesNumber = endNumber;
+        }
+    }
+
+    //创建单个课程视图
+    private void createItemCourseView(final Course course) {
+        int getDay = course.getDay();
+        if ((getDay < 1 || getDay > 7) || course.getStart() > course.getEnd()) {
+            Toast.makeText(getActivity(), "星期几没写对,或课程结束时间比开始时间还早~~", Toast.LENGTH_LONG).show();
+        }
+        else {
+            int dayId = 0;
+            switch (getDay) {
+                case 1: dayId = R.id.monday; break;
+                case 2: dayId = R.id.tuesday; break;
+                case 3: dayId = R.id.wednesday; break;
+                case 4: dayId = R.id.thursday; break;
+                case 5: dayId = R.id.friday; break;
+                case 6: dayId = R.id.saturday; break;
+                case 7: dayId = R.id.weekday; break;
+            }
+            day = root.findViewById(dayId);
+
+            int height = 180;
+            View v = inflater.inflate(R.layout.course_card, null); //加载单个课程布局
+            v.setY(height * (course.getStart()-1)); //设置开始高度,即第几节课开始
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams
+                    (ViewGroup.LayoutParams.MATCH_PARENT,(course.getEnd()-course.getStart()+1)*height - 8); //设置布局高度,即跨多少节课
+            v.setLayoutParams(params);
+            TextView text = v.findViewById(R.id.text_view);
+            text.setText(course.getCourseName() + "\n" + course.getTeacher() + "\n" + course.getClassRoom()); //显示课程名
+            day.addView(v);
+            //长按删除课程
+            v.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View v) {
+                    v.setVisibility(View.GONE);//先隐藏
+                    day.removeView(v);//再移除课程视图
+                    return true;
+                }
+            });
+        }
     }
 }
